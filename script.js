5dcc2f8d4cda998bd554275adf199c5b3b39b293
pallavranpise <runpsaypallove@gmail.com>
2022-08-25 01:18:55 +0530

    displays result

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let opop = '';
 let firstChosen = false;
 let secondChosen = false;
 
+const ac = document.querySelector("#clear");
+const display = document.querySelector(".display");
+const operat = document.querySelectorAll(".operators");
+const digits = document.querySelectorAll(".digits");
+
+
+
 function resetMem(){
     operand1 = 0;
     operand2 = 0;
@@ -24,29 +31,35 @@ const operate = (operation,a,b)=>{
 
 
 
-const operat = document.querySelectorAll(".operators");
 
 operat.forEach(key=>key.addEventListener("click",(op)=>{
     opop = op.target.innerText;
-    
+    refreshDisplay(" "+opop+" ");
 }));
 
-const digits = document.querySelectorAll(".digits");
 
 digits.forEach(key=>key.addEventListener("click",(digit)=>{
     if(firstChosen){
         operand2 = digit.target.innerText;
         secondChosen = true;
+        refreshDisplay(operand2);
     }else{
         operand1 = digit.target.innerText;
         firstChosen = true;
+        refreshDisplay(operand1);
     }
     if(firstChosen && secondChosen && opop){
-        console.log(operate(opop,operand1,operand2));
+        // console.log(operate(opop,operand1,operand2));
+        refreshDisplay((" = "+operate(opop,operand1,operand2)));
     }
-
-
 }));
+
+ac.addEventListener("click",(trivial)=>refreshDisplay(trivial,true));
+
+function refreshDisplay(touch,clearDisplay=false){
+    display.innerText += touch;
+    if(clearDisplay)display.innerText = "";
+}
 
 
 
