522c5b33de7d89a51b38acf6db46b9a8acc28979
pallavranpise <runpsaypallove@gmail.com>
2022-08-26 01:21:03 +0530

    added backspace functionality

--- script.js
+++ script.js
@@ -12,91 +12,86 @@ const display = document.querySelector(".display");
 const operat = document.querySelectorAll(".operators");
 const digits = document.querySelectorAll(".digits");
 
-
-
-function resetMem(){
-    operand1 = "";
-    operand2 = "";
-    opop = "";
-    firstChosen = false;
-    secondChosen = false;
-    gotten = "";
-
+function resetMem() {
+  operand1 = "";
+  operand2 = "";
+  opop = "";
+  firstChosen = false;
+  secondChosen = false;
+  gotten = "";
 }
 
-const operate = (operation,a,b)=>{
-    let x;
-    if(operation==="+")x= Number(a)+Number(b);
-    else if(operation==="-")x= a-b;
-    else if(operation==="*")x= a*b;
-    else if(operation==="/")x= Math.round((a/b)*10000)/10000;
-    gotten = x;
-    return x;
-}
+const operate = (operation, a, b) => {
+  let x;
+  if (operation === "+") x = Number(a) + Number(b);
+  else if (operation === "-") x = a - b;
+  else if (operation === "*") x = a * b;
+  else if (operation === "/")x = Math.round((a / b) * 10000) / 10000;
+  if(isNaN(x)){gotten = "lmao"; return gotten;}
+  gotten = x;
+  return x;
+};
 
-operat.forEach(key=>key.addEventListener("click",(op)=>{
-    if(firstChosen && secondChosen && opop){
-        cleanDisplay();
-        gotten = operate(opop,operand1,operand2);
-        appendDisply(gotten);
-        operand1 = gotten;
-        firstChosen = true;
-        operand2 = "";
-        secondChosen = false;
-        opop = "";
+operat.forEach((key) =>
+  key.addEventListener("click", (op) => {
+    if (firstChosen && secondChosen && opop) {
+      cleanDisplay();
+      gotten = operate(opop, operand1, operand2);
+      appendDisply(gotten);
+      operand1 = gotten;
+      firstChosen = true;
+      operand2 = "";
+      secondChosen = false;
+      opop = "";
     }
     opop = op.target.innerText;
 
     firstChosen = true;
     secondChosen = false;
     operand2 = "";
-    
-}));
+  })
+);
 
-
-digits.forEach(key=>key.addEventListener("click",(digit)=>{
+digits.forEach((key) =>
+  key.addEventListener("click", (digit) => {
     takeNum(digit);
-}));
+  })
+);
 
-function takeNum(n){
-    if(!firstChosen){
-        operand1 += (n.target.innerText);
-        appendDisply(n.target.innerText);
-    }else if(firstChosen){
-        if(firstChosen && !secondChosen)cleanDisplay();
-        operand2 += n.target.innerText;
-        secondChosen = true;
-        appendDisply(n.target.innerText);
-    }
+function takeNum(n) {
+  if (!firstChosen) {
+    operand1 += n.target.innerText;
+    appendDisply(n.target.innerText);
+  } else if (firstChosen) {
+    if (firstChosen && !secondChosen) cleanDisplay();
+    operand2 += n.target.innerText;
+    secondChosen = true;
+    appendDisply(n.target.innerText);
+  }
 }
 
+ac.addEventListener("click", () => {
+  resetMem();
+  cleanDisplay();
+});
 
-    ac.addEventListener("click",()=>{
-        resetMem();
-        cleanDisplay();
-    });
+equalsTo.addEventListener("click", () => {
+  if (firstChosen && secondChosen && opop) {
+    cleanDisplay();
+    gotten = operate(opop, operand1, operand2);
+    appendDisply(gotten);
+    operand1 = gotten;
+    firstChosen = true;
+    operand2 = "";
+    secondChosen = false;
+    opop = "";
+  }
+});
 
-equalsTo.addEventListener("click",(e)=>{if(firstChosen && secondChosen && opop){
-    if(firstChosen && secondChosen && opop){
-        cleanDisplay();
-        gotten = operate(opop,operand1,operand2);
-        appendDisply(gotten);
-        operand1 = gotten;
-        firstChosen = true;
-        operand2 = "";
-        secondChosen = false;
-        opop = "";
-    }
-    
-}})
-
-
-
-function appendDisply(touch){
-    display.innerText += touch;
+function appendDisply(touch) {
+  display.innerText += touch;
 }
 
-function cleanDisplay(){
-    display.innerText = "";
+function cleanDisplay() {
+  display.innerText = "";
 }
-
