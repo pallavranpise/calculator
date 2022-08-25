d0bb6ca6339f36c1a5b2a62c68c2e7dfcb82d93c
pallavranpise <runpsaypallove@gmail.com>
2022-08-26 01:52:34 +0530

    added floats input

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let opop = "";
 let gotten = "";
 let firstChosen = false;
 let secondChosen = false;
-let once = true;
+let pointer1flag = true;
+let pointer2flag =true;
 
 const equalsTo = document.querySelector("#isEqualsTo");
 const ac = document.querySelector("#allclear");
@@ -12,6 +13,7 @@ const display = document.querySelector(".display");
 const operat = document.querySelectorAll(".operators");
 const digits = document.querySelectorAll(".digits");
 const backspace = document.querySelector("#backspace");
+const point = document.querySelector("#point");
 
 function resetMem() {
   operand1 = "";
@@ -20,6 +22,8 @@ function resetMem() {
   firstChosen = false;
   secondChosen = false;
   gotten = "";
+  pointer1flag = true;
+  pointer2flag =true;
 }
 
 const operate = (operation, a, b) => {
@@ -28,7 +32,7 @@ const operate = (operation, a, b) => {
   else if (operation === "-") x = a - b;
   else if (operation === "*") x = a * b;
   else if (operation === "/")x = Math.round((a / b) * 10000) / 10000;
-  if(isNaN(x)){gotten = "lmao"; return gotten;}
+  if(isNaN(x)){gotten = "kek"; return gotten;}
   gotten = x;
   return x;
 };
@@ -44,9 +48,12 @@ operat.forEach((key) =>
       operand2 = "";
       secondChosen = false;
       opop = "";
+      pointer1flag = false;
+      pointer2flag = true;
     }
     opop = op.target.innerText;
-
+    pointer1flag = false;
+    pointer2flag = true;
     firstChosen = true;
     secondChosen = false;
     operand2 = "";
@@ -91,6 +98,28 @@ backspace.addEventListener("click",()=>{
         appendDisply(operand2);
     }
 });
+
+point.addEventListener("click",()=>{
+    if(!firstChosen && pointer1flag){
+        operand1 = String(operand1)+".";
+        cleanDisplay();
+        appendDisply(operand1);
+        pointer1flag = false;
+    }else if(firstChosen && !secondChosen  && pointer1flag){
+        operand1 = String(operand1)+".";
+        cleanDisplay();
+        appendDisply(operand1);
+        pointer1flag = false;
+    }else if(firstChosen && secondChosen  && pointer2flag){
+        operand2 = String(operand2)+".";
+        cleanDisplay();
+        appendDisply(operand2);
+        pointer2flag = false;
+
+    }
+});
+
+
 
 equalsTo.addEventListener("click", () => {
   if (firstChosen && secondChosen && opop) {
